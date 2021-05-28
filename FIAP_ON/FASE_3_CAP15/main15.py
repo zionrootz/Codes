fe8671294558b9b1898fe85d4a331369f5352e54
Ney Torres <ney.torres@hotmail.com>
2021-05-28 00:12:08 -0300

    atualizando pastas

--- FIAP_ON/FASE_3_CAP15/main15.py
+++ FIAP_ON/FASE_3_CAP15/main15.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-#URL: https://dc1-2021.glitch.me/getHash
+#URL da API: https://dc1-2021.glitch.me/getHash
 #RM: 87856
 #Senha: vAwCNf2nbe
 #Obs: Antes de começar a acessar a API, acesse https://dc1-2021.glitch.me/ com seu navegador web primeiro. Como o serviço é gratuito, ele fica 'dormente' quando ninguém está usando...
@@ -10,7 +10,6 @@ dictjson = {
 "rm": 87856,
 "senha": "vAwCNf2nbe"}
 
-#r = requests.post('https://dc1-2021.glitch.me/getHash', json=dictjson)
 r = requests.post('https://dc1-2021.glitch.me/getHash', dictjson)
 r.status_code
 textdict = json.loads(r.text)
