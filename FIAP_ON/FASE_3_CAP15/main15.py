90caadedc7981bf500907d676cfa8c2fbd565581
Ney Torres <ney.torres@hotmail.com>
2021-05-30 03:42:02 -0300

    Fim main15

--- FIAP_ON/FASE_3_CAP15/main15.py
+++ FIAP_ON/FASE_3_CAP15/main15.py
@@ -6,21 +6,27 @@ import json
 #Senha: vAwCNf2nbe
 #Obs: Antes de começar a acessar a API, acesse https://dc1-2021.glitch.me/ com seu navegador web primeiro. Como o serviço é gratuito, ele fica 'dormente' quando ninguém está usando...
 
+#Criando dicionario de dados contendo as informações do meu RM e senha recebidos para esta atividade
 my_rm_pass = { 
     "rm": 87856, 
     "senha": "vAwCNf2nbe"
 }
 
-msg_erro = {"erro":"RM/senha inválidos"}
-
+# Chamando API utilizando função Post do Módulo Requests
 r = requests.post('https://dc1-2021.glitch.me/getHash', my_rm_pass)
+# Obtendo Status Code
 r.status_code
 
+# Transformando o texto em json recebido da API em dicionario de dados com a função loads do Módulo Json
 textdict = json.loads(r.text)
 
-#print("Status Code: {}".format(r.status_code))
+# Declarando uma varivel com o conteudo da mensagem de erro para comparação com texto recebido
+msg_erro = {"erro":"RM/senha inválidos"}
+
+# Se o texto recebido é diferente da mensagem de erro exibe o Nome e Senha Hash recebidos da API
 if textdict != msg_erro:
     print("Nome: {}".format(textdict["nome"]))
     print("Senha_hash: {}".format(textdict["hash"]))
+# Senão exibe a mensagem de erro recebida da API
 else:
     print(textdict['erro'])
