e275f12e9d52372d2e2a09b74d2ba5a1c43bfa05
Ney Torres <ney.torres@hotmail.com>
2021-06-11 02:28:42 -0300

    show

--- FIAP_ON/FASE_4_CTF/T3-05.py
+++ FIAP_ON/FASE_4_CTF/T3-05.py
@@ -1,16 +1,24 @@
 #Criar uma estrutura em Python que multiplique apenas os números primos presentes nesta lista, ou seja, 
-# NÃO MULTIPLICA o 34 (por ser divisível por 2), multiplica o 3 (que é primo), o 1 (sim, o um vai entrar) e por aí vai … a FLAG é o resultado desta multiplicação.
+# NÃO MULTIPLICA o 34 (por ser divisível por 2), multiplica o 3 (que é primo), o 1 (sim, o um vai entrar) e por aí vai … 
+# a FLAG é o resultado desta multiplicação.
 
 numeros = [34, 3, 1, 51, 6, 33, 23, 2, 29, 5, 33, 1, 7, 65, 4, 12, 5, 7, 46, 3, 41, 15, 3, 23, 2, 1, 3, 21, 35, 4, 6, 2, 13, 5, 14, 10, 3, 25, 7, 13, 14, 7, 5, 33, 21]
 
-
-#for count in range(2,n):
 valor=1
+ct=0
+
+# Um numero só é primo se for divisivel unica e exclusivamente por 1 e por ele mesmo, totalizando apenas 2 possiveis divisoes
 for num in numeros:
-    if (num %2 == 1):
-        print(num,"É primo")
+    for c in range(1,num + 1):
+        if num % c == 0:
+            ct +=1
+
+    if ct == 2:
+        print('Número', num, 'é primo')
         valor=valor*num
+        ct = 0
     else:
-        print(num,"Não é primo")
+        print('Número', num, 'não é primo')
+        ct = 0
 
 print("Valor final: ", valor)
